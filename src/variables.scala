d758add039058d450ea6f4d009e9b7d8dbc95561
valentk777 <valentk777@gmail.com>
2020-05-30 21:42:54 +0300

    variables

---
+++ src/variables.scala
@@ -0,0 +1,15 @@
+object variables {
+  def main(args: Array[String]) {
+    val i = 4
+    val d = 4.0
+    val s = "HackerRank "
+
+    val ni = scala.io.StdIn.readInt()
+    val nd = scala.io.StdIn.readDouble()
+    val ns = scala.io.StdIn.readLine()
+
+    println(i + ni)
+    println(d + nd)
+    println(s + ns)
+  }
+}
