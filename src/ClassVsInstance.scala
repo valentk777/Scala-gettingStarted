e923c94a04dc6243ff015dc64d91dc6e4a93e1f4
valentk777 <valentk777@gmail.com>
2020-05-30 22:48:57 +0300

    strings and loops

--- src/ClassVsInstance.scala
+++ src/ClassVsInstance.scala
@@ -34,14 +34,12 @@ class Person {
 object Solution {
 
   def main(args: Array[String]) {
-    var T = scala.io.StdIn.readInt()
-    var i = 0
-    for (i <- 1 to T) {
-      var age = scala.io.StdIn.readInt()
-      var p = new Person(age)
+    val T = scala.io.StdIn.readInt()
+    for (_ <- 1 to T) {
+      val age = scala.io.StdIn.readInt()
+      val p = new Person(age)
       p.amIOld()
-      var j = 0
-      for (j <- 1 to 3) {
+      for (_ <- 1 to 3) {
         p.yearPasses()
       }
       p.amIOld()
---
+++ src/Strings.scala
@@ -0,0 +1,19 @@
+object Strings {
+  def main(args: Array[String]) {
+    val stdin = scala.io.StdIn
+    val t = stdin.readInt()
+    for (_ <- 1 to t) {
+      val s = stdin.readLine()
+      var s0 = ""
+      var s1 = ""
+      for (i: Int <- 0 until s.length) {
+        if (i % 2 == 0) {
+          s0 += s.charAt(i)
+        } else {
+          s1 += s.charAt(i)
+        }
+      }
+      println(s"$s0 $s1")
+    }
+  }
+}
